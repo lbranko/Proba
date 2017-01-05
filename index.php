d382a283a4880113ff18ac618ccf52a188a994f0
lbranko <lukac.branko@gmail.com>
2017-01-05 21:00:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,47 +11,47 @@ abstract class Vehicle {
 
     function stop() {
         $this->driving = 0;
-        echo "Vehicle stopped!";
+        echo "Vehicle stopped!<br>";
     }
 
     function drive() {
         if ($this->numPerson <= $this->maxPerson && $this->numPerson > 0) {
             $this->driving = 1;
-            echo "Vehicle started!";
+            echo "Vehicle started!<br>";
         } else if ($this->numPerson <= 0) {
-            echo "Cannot start vehicle, vehicle is empty!";
+            echo "Cannot start vehicle, vehicle is empty!<br>";
         } else {
-            echo "Cannot start vehicle, too many people!";
+            echo "Cannot start vehicle, too many people!<br>";
         }
     }
 
     function addPerson() {
         if ($this->driving == 0) {
             $this->numPerson++;
-            echo "Person added!";
+            echo "Person added!<br>";
         } else {
-            echo "Cannot add person, vehicle is driving!";
+            echo "Cannot add person, vehicle is driving!<br>";
         }
     }
 
     function removePerson() {
         if ($this->numPerson > 0 && $this->driving == 0) {
             $this->numPerson--;
-            echo "Person removed";
+            echo "Person removed<br>";
         } else if ($this->numPerson <= 0) {
-            echo "Cannot remove person, vehicle is empty!";
+            echo "Cannot remove person, vehicle is empty!<br>";
         } else {
-            echo "Cannot remove person, vehicle is driving!";
+            echo "Cannot remove person, vehicle is driving!<br>";
         }
     }
 
     final public function getDescription() {
         switch ($this->maxPerson) {
             case "1":
-                echo"Human-powered, pedal-driven, single-track vehicle";
+                echo"Human-powered, pedal-driven, single-track vehicle<br>";
                 break;
             case "5":
-                echo "Wheeled, self-powered motor vehicle used for transportation";
+                echo "Wheeled, self-powered motor vehicle used for transportation<br>";
                 break;
         }
     }
@@ -63,7 +63,7 @@ class Car extends Vehicle {
     public $maxPerson = 5;
 
     function getMaxPeople() {
-        echo "Max number of person is " . $this->maxPerson;
+        echo "Max number of person is " . $this->maxPerson . "<br>";
     }
 
 }
@@ -73,7 +73,39 @@ class Bicycle extends Vehicle {
     public $maxPerson = 1;
 
     function getMaxPeople() {
-        echo "Max number of person is " . $this->maxPerson;
+        echo "Max number of person is " . $this->maxPerson . "<br>";
     }
 
 }
+
+$car = new Car(4);
+echo $car->getDescription(); // Wheeled, self-powered motor vehicle used for transportation
+echo $car->getMaxPeople();
+echo $car->drive(); // Vehicle started!
+echo $car->addPerson(); // Cannot add person, vehicle is driving!
+echo $car->stop(); // Vehicle stopped!
+echo $car->addPerson(); // Person added!
+echo $car->addPerson(); // Person added!
+echo $car->drive(); // Cannot start vehicle, too many people!
+echo $car->removePerson();
+echo $car->drive();
+echo $car->removePerson();
+echo $car->stop();
+
+echo "<hr>";
+
+$bike = new Bicycle(2);
+echo $bike->getDescription(); // Human-powered, pedal-driven, single-track vehicle
+echo $bike->getMaxPeople();
+echo $bike->drive(); // Cannot start vehicle, too many people!
+echo $bike->removePerson(); // Person removed!
+echo $bike->drive(); // Vehicle started!
+echo $bike->removePerson(); // Cannot remove person, vehicle is driving!
+echo $bike->stop(); // Vehicle stopped!
+echo $bike->removePerson(); // Person removed!
+echo $bike->drive(); // Cannot start vehicle, vehicle is empty!
+echo $bike->addPerson(); // Person added!
+echo $bike->drive(); // Vehicle started!
+echo $bike->stop(); // Vehicle stopped!
+echo $bike->removePerson(); // Person removed!
+echo $bike->removePerson(); // Cannot remove person, vehicle is empty!
